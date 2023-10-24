58728342082905c37be918a2e2b8d0fe1e117b48
Cory House <housecor@gmail.com>
2023-10-24 10:47:22 -0400

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import "./App.css";
 import { foods } from "./food";
 
+// Exercise: Display food info
+// and style info and card via Tailwind
 function App() {
   return (
     <>
       <h1>Menu</h1>
-      {foods.length}
+      <div className="flex flex-wrap">
+        {foods.map((food) => (
+          <div
+            className="bg-slate-200 shadow-lg m-2 w-96 border p-2 rounded"
+            key={food.id}
+          >
+            <h2>{food.name}</h2>
+            <p className="font-bold">${food.price}</p>
+            <p>{food.description}</p>
+            <img
+              className="h-48"
+              src={"images/" + food.image}
+              alt={food.name}
+            />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
